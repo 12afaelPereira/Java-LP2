8bd897d02c85e1c2f989ce23793928693c9f1ea0
Rafael Pereira <rafaelpereira2.0@outlook.com>
2021-03-23 18:33:07 -0300

    add Cat class with getter and setter

---
+++ Instancia de classe 2/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+    public static void main(String args[]){
+
+        Cat rufus = new Cat();
+        rufus.setName("Rufus");
+        System.out.println(rufus.getName());
+
+        Cat bubles = new Cat();
+        bubles.setName("Bubles");
+        System.out.println(bubles.getName());
+    }
+}
